9663906170ea4fdab0ce5913572de7440f68bb27
Colin Stark <i1672937@mvrht.net>
2017-07-12 16:41:18 -0600

    main and music

---
+++ js/main.js
@@ -0,0 +1,14 @@
+console.log(origin);
+order 1;
+var favsong== (
+  song ("humbling river");
+  artist ("unknown");
+console.log(favsong);
+var tbody=document.querySeletor("tbody");
+tbody.backgroundColor= ,# 093600
+console.log(tbody);
+tr appendchild(tdi)
+(
+
+)
+console.log(tr); 
